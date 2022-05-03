54166d1ee1058488ab351cb1c84335e2dd75dd14
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-05-03 22:59:55 +0600

    start-login-from

--- src/Pages/Home/Login/Login.js
+++ src/Pages/Home/Login/Login.js
@@ -1,9 +1,22 @@
 import React from "react";
-
+import { useForm } from "react-hook-form";
 const Login = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <div>
-      <h2>This is login component</h2>
+    <div className="d-flex">
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input {...register("firstName", { required: true, maxLength: 20 })} />
+        <input {...register("lastName", { pattern: /^[A-Za-z]+$/i })} />
+        <input type="number" {...register("age", { min: 18, max: 99 })} />
+        <input type="submit" />
+      </form>
     </div>
   );
 };
