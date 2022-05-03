66c1cb7275b681b152a1c4e19f83672e4287d1a0
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-05-03 19:08:00 +0600

    add-carusel

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,17 +1,21 @@
 import React from "react";
 import "./Banner.css";
 import logo from "../../../image/book-3.png";
-import logo1 from "../../../image/book-2.png";
-import logo2 from "../../../image/book-9.png";
+import logo1 from "../../../image/book-1.png";
+import logo2 from "../../../image/book-2.png";
+import logo3 from "../../../image/book-9.png";
+import logo4 from "../../../image/book3.png";
+import logo5 from "../../../image/book5.png";
+import logo6 from "../../../image/book7.png";
 import banner from "../../../image/stand.png";
-import { Button } from "react-bootstrap";
+import { Button, Carousel } from "react-bootstrap";
 
 const Banner = () => {
   return (
     <div className="banar ">
-      <div className=" row ">
+      <div className=" row align-items-center">
         <div className="content ">
-          <div className="mb-4">Get You New</div>
+          <h2>Get You New</h2>
           <h2>Book Collections</h2>
           <p>
             Ther are many variations of passages of lorem lpsum available ,but
@@ -23,14 +27,33 @@ const Banner = () => {
         </div>
 
         <div className="swiper mySwiper book-slider ">
-          <div className="swiper-wrapper">
-            <a href="#">
-              <img src={logo} alt="" />
-            </a>
-            <a href="#">
-              <img src={logo2} alt="" />
-            </a>
-          </div>
+          <div className="swiper-wrapper"></div>
+          <Carousel>
+            <Carousel.Item>
+              <a href="#">
+                <img src={logo} alt="" />
+              </a>
+              <a href="#">
+                <img src={logo1} alt="" />
+              </a>
+            </Carousel.Item>
+            <Carousel.Item>
+              <a href="#">
+                <img src={logo2} alt="" />
+              </a>
+              <a href="#">
+                <img src={logo3} alt="" />
+              </a>
+            </Carousel.Item>
+            <Carousel.Item>
+              <a href="#">
+                <img src={logo4} alt="" />
+              </a>
+              <a href="#">
+                <img src={logo5} alt="" />
+              </a>
+            </Carousel.Item>
+          </Carousel>
           <img className=" stand" src={banner} alt="" />
         </div>
       </div>
